e62b5d253cd882f0d7a1ae33ea310cefc1318e2c
Oluwaseke Tanimola <ooluwaseke@gmail.com>
2022-06-08 15:37:55 +0100

    Set up the project
    Created the backend files and installed required dependencies.(This isn't a major commit just majorly for backup purposes)

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const DATABASE_CONNECTION = "";
